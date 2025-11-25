d392dd3bb68485328c51407af9b4971471213484
maneesha <maneesha@opensource.lk>
2025-11-25 18:52:50 +0530

    add timeout

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	server := &JMAPServer{}
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      server,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
 	log.Println("JMAP server starting on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", &JMAPServer{}))
+	log.Fatal(srv.ListenAndServe())
 }
