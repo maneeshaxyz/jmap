ab29d9b51f4ff733d28e086e72c9061bfc25843a
maneesha <maneesha@opensource.lk>
2025-11-29 23:53:04 +0530

    added custom logger and http.Server struct so it uses errorLog

--- cmd/jmapd/main.go
+++ cmd/jmapd/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,12 +12,20 @@ func main() {
 	port := flag.String("port", ":8080", "HTTP port")
 	flag.Parse()
 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/get/resource", getHandler)
 	mux.HandleFunc("/post/resource", postHandler)
 
-	log.Printf("Starting server on %s", *port)
-	err := http.ListenAndServe(*port, mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", *port)
+	srv := &http.Server{
+		Addr:     *port,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
