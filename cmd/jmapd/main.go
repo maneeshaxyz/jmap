97081ce66eba628f801c57d63aaac667eaecdad1
maneesha <maneesha@opensource.lk>
2025-11-29 23:43:07 +0530

    add flag for port

--- cmd/jmapd/main.go
+++ cmd/jmapd/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	port := flag.String("port", ":8080", "HTTP port")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/get/resource", getHandler)
 	mux.HandleFunc("/post/resource", postHandler)
 
-	log.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Printf("Starting server on %s", *port)
+	err := http.ListenAndServe(*port, mux)
 	log.Fatal(err)
 }
