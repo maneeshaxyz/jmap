497504db32b4c46a7988b4c6b30546e4600d0b1d
maneesha <maneesha@opensource.lk>
2026-03-18 20:26:24 +0530

    adding error types.

--- internal/core/errors.go
+++ internal/core/errors.go
@@ -1,9 +1,15 @@
 package jmap
 
+const (
+	ErrorTypeNotJSON           = "urn:ietf:params:jmap:error:notJSON"
+	ErrorTypeNotRequest        = "urn:ietf:params:jmap:error:notRequest"
+	ErrorTypeUnknownCapability = "urn:ietf:params:jmap:error:unknownCapability"
+	ErrorTypeLimit             = "urn:ietf:params:jmap:error:limit"
+)
+
 type JmapError struct {
-	Type        string `json:"type"`
-	Status      int    `json:"status,omitempty"`
-	Detail      string `json:"detail,omitempty"`
-	Limit       string `json:"limit,omitempty"`
-	Description string `json:"description,omitempty"`
+	Type   string `json:"type"`
+	Status int    `json:"status,omitempty"`
+	Detail string `json:"detail,omitempty"`
+	Limit  string `json:"limit,omitempty"`
 }
